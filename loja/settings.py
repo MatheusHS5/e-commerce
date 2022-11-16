361a0e2a5abed8e11982c3594a062456edc4deaf
MatheusHS5 <mhztecnologia@gmail.com>
2022-11-15 21:48:38 -0300

    Configurado o restante das apps no settings

--- loja/settings.py
+++ loja/settings.py
@@ -32,7 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'produto', 
+    'produto',
+    'pedido',
+    'perfil',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

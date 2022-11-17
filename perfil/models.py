2a2ebe882e6ca652e194b827a1935293399f4b85
MatheusHS5 <mhztecnologia@gmail.com>
2022-11-16 22:56:02 -0300

    Tratamento de exceções no formulário de pedidos

--- perfil/models.py
+++ perfil/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ValidationError
+
+import re
+from utils.validacpf import valida_cpf
 
 class Perfil(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuário')
     idade = models.PositiveIntegerField()
-    data_nascimento = models.DateField()
-    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField(verbose_name='Data de nascimento')
+    cpf = models.CharField(max_length=11, verbose_name='CPF')
     endereco = models.CharField(max_length=50)
-    numero = models.CharField(max_length=5)
+    numero = models.CharField(max_length=5, verbose_name='Número')
     complemento = models.CharField(max_length=30)
     bairro = models.CharField(max_length=30)
-    cep = models.CharField(max_length=8)
+    cep = models.CharField(max_length=8, verbose_name='CEP')
     cidade = models.CharField(max_length=30)
     estado = models.CharField(
         max_length=2,
@@ -47,10 +51,20 @@ class Perfil(models.Model):
     )
 
     def __str__(self):
-        return f'{self.usuario.first_name} {self.usuario.last_name}'
+        return f'{self.usuario}'
 
     def clean(self):
-        pass
+        error_messages = {}
+
+        if not valida_cpf(self.cpf):
+            error_messages['cpf'] = 'Digite um CPF válido!'
+
+        if re.search(r'[^0-9]', self.cep) or len(self.cep) < 8:
+            error_messages['cep'] = 'CEP inválido! Digite os 8 dígitos do seu CEP!'
+
+        if error_messages:
+            raise ValidationError(error_messages)
+
 
     class Meta:
         verbose_name = 'Pefil'

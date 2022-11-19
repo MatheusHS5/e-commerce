1f9160941475cecd1c6850964f5d74df855c2ac0
MatheusHS5 <mhztecnologia@gmail.com>
2022-11-18 23:13:44 -0300

    Criado as Views e URLs das Apps

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+'''
+REABILITAR ESSA CLASS E REMOVAR A SEGUINTE DEPOIS:
+class ListaProdutos(ListView):
+    pass
+'''
+
+class ListaProdutos(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('ListaProdutos')
+
+class DetalheProduto(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('DetalheProduto')
+
+class AdicionarAoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('AdicionarAoCarrinho')
+
+class RemoverDoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('RemoverDoCarrinho')
+
+class Carrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Carrinho')
+
+class Finalizar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Finalizar')
